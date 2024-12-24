12ca92165cdd6619e9a61b5756bfd80338440ff3
Willian Jesus Steinbach <eng.civil.willian@gmail.com>
2024-12-24 17:26:06 -0300

    version 0.2

--- New Folder/Tempo.java
+++ New Folder/Tempo.java
@@ -3,18 +3,34 @@ package exercicio.java;
 public class Tempo {
 
     public static void main(String[] args) {
-        boolean noite = true;
-        boolean chuva = false;
+        boolean noite = false;
+        boolean chuva = true;
+        int flores = 30;
+        int ano = 2069;
+        int tempo = 30;
+        int resultado = 0;
+        int resultado_flores = 0;
+        float temperatura_1 = 5;
+        float temperatura_2 = 40;
+        float resultado_temp = 0;
         
         if (noite) {
-            System.out.println("Está uma noite muito fria.\n");
+            resultado = ano + tempo;
+            resultado_flores = flores + tempo;
+            resultado_temp = temperatura_1 - temperatura_2;
+            System.out.println("O ano é " + resultado + ", está de noite e a temperatura é " + resultado_temp + "ºC.\nEstá muito frio e o total de flores no planeta Terra é de " + resultado_flores + ".\n");
         } else {
-            System.out.println("Está de dia e faz calor\n");
+            resultado = ano - tempo;
+            resultado_flores = flores - tempo;
+            resultado_temp = temperatura_2 + temperatura_1;
+            System.out.println("O ano é " + resultado + ", está de dia e a temperatura é " + resultado_temp + "ºC.\nEstá muito calor e o total de flores no planeta Terra é de " + resultado_flores + ".\n");
         }
         
         if (chuva) {
+            resultado = ano + tempo;
             System.out.println("Está chovendo.\n");
         } else {
+            resultado = ano - tempo;
             System.out.println("Não está chovendo.\n");
         }
     }
